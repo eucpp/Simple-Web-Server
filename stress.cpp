c3c5d4c1565d4673a72e60cd9dc2ac0dd9d820e7
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2017-06-16 20:31:32 +0300

    stress test

--- stress.cpp
+++ stress.cpp
@@ -1,3 +1,7 @@
+#include <chrono>
+#include <thread>
+#include <vector>
+
 #include "server_http.hpp"
 #include "client_http.hpp"
 
@@ -18,6 +22,10 @@ using namespace allocgc::serial;
 typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
 typedef SimpleWeb::Client<SimpleWeb::HTTP> HttpClient;
 
+std::chrono::steady_clock::time_point now() {
+    return std::chrono::steady_clock::now();
+}
+
 #if !(defined(PRECISE_GC_SERIAL) || defined(PRECISE_GC_CMS))
 template <typename Function, typename... Args>
     std::thread create_thread(Function&& f, Args&&... args)
@@ -29,21 +37,49 @@ template <typename Function, typename... Args>
 int main(int argc, const char* argv[]) {
 
     #if defined(PRECISE_GC_SERIAL) || defined(PRECISE_GC_CMS)
-    //        enable_logging(gc_loglevel::DEBUG);
+//        enable_logging(gc_loglevel::INFO);
+        set_threads_available(1);
         register_main_thread();
     #endif
 
     HttpServer server;
     server.config.port=8080;
 
-    server.resource["^/string$"]["POST"]=[](ptr_t(HttpServer::Response) response, ptr_t(HttpServer::Request) request) {
-        //Retrieve string:
+    server.resource["^/test$"]["GET"]=[](ptr_t(HttpServer::Response) response, ptr_t(HttpServer::Request) request) {
         auto content=request->content.string();
-        //request->content.string() is a convenience function for:
-        //stringstream ss;
-        //ss << request->content.rdbuf();
-        //string content=ss.str();
-
         *pin(response) << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
     };
+
+    std::thread server_thread = create_thread([&server](){
+        server.start();
+    });
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    size_t threads_num = std::thread::hardware_concurrency();
+
+    auto tm = now();
+
+    std::vector<std::thread> threads(threads_num);
+    for (auto& thread: threads) {
+        thread = create_thread([] () {
+            const size_t ITER_COUNT = 1000;
+
+            HttpClient client("localhost:8080");
+            for (size_t i = 0; i < ITER_COUNT; ++i) {
+                auto rsp = client.request("GET", "/test");
+            }
+        });
+    };
+
+    for (auto& thread: threads) {
+        thread.join();
+    };
+
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now() - tm).count();
+    std::cout << "Completed in " << elapsed << " ms" << std::endl;
+
+    server.stop();
+    server_thread.join();
+
+    return 0;
 }
